ae860db553141b20aa63dc0901d810f5d9192fb5
Nick Rosa <nicholascastrorosa@gmail.com>
2020-02-10 17:08:50 -0700

    Base

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express'),
-      session = require('express-session'),
       massive = require('massive'),
+      session = require('express-session'),
       {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
       app = express();
 
-app.use(express.json());
-app.use(session({
-    resave: false,
-    saveUninitialized: true,
-    secret: SESSION_SECRET,
-    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}
-}))
+app.use(express.json())
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: true,
+        secret: SESSION_SECRET,
+        cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}
+    })
+)
+
 
 massive(CONNECTION_STRING).then(db => {
     app.set('db', db)
     console.log('db connected')
     const port = SERVER_PORT;
-    app.listen(port, () => console.log(`Building on port: ${port}`))
-});
+    app.listen(port, () => console.log(`Building on port: ${port}`));
+})
 
 // ENDPOINTS
